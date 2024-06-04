9cd9f58522357b43d32de0eff05ed04b99600ef1
matheusgomes28 <matheusgarcia28@gmail.com>
2024-06-04 22:41:18 +0100

    Stream: adding id functions for new entity map

--- ecs_app/main.cpp
+++ ecs_app/main.cpp
@@ -3,9 +3,46 @@ import maecs;
 #include <raylib.h>
 
 #include <array>
+#include <chrono>
+#include <thread>
+#include <variant>
 
 namespace
 {
+    struct Position
+    {
+        int x;
+        int y;
+    };
+
+    struct Rect
+    {
+        int top;
+        int left;
+        int width;
+        int height;
+    };
+
+    struct Circle
+    {
+        int center_x;
+        int center_y;
+        int radius;
+    };
+
+    struct Colour
+    {
+        // refer to the colour pallete index
+        std::size_t id;
+    };
+
+    struct Triangle
+    {
+        std::size_t a;
+        std::size_t b;
+        std::size_t c;
+    };
+
     static constexpr std::array<Color, 16> colour_table{{
         {0, 0, 0, 255}, // Black
         {255, 255, 255, 255}, // White
@@ -27,62 +64,72 @@ namespace
 
 
 // TODO mutex to protect memory
-// bool draw(emulator::Cpu& cpu)
-// {
-//     InitWindow(512, 512, "6502 Graphics");
-//     while (!WindowShouldClose())
-//     {
-//         BeginDrawing();
-//         ClearBackground(BLACK);
-//         // 0200 - 05FF :
-//         for (int i = 0x0200; i < 0x0600; ++i)
-//         {
-//             auto const colour_id = cpu.mem[i] % 16;
-//             auto const& colour   = colour_table[colour_id];
-
-//             // Draw this colour rectangle
-//             auto const rel_pos = i - 0x0200;
-//             auto const row     = rel_pos / 32;
-//             auto const col     = rel_pos % 32;
-//             DrawRectangle(col * 16, row * 16, 16, 16, colour);
-//         }
-//         EndDrawing();
-
-//         // Make sure we only draw at most 30 times per second
-//         std::this_thread::sleep_for(std::chrono::milliseconds(1000 / 30));
-//     }
-
-//     return true;
-// }
-
-struct Position
+bool draw(maecs::Registry<Circle, Rect, Position, Colour>const & registry)
 {
-    int x;
-    int y;
-};
+    InitWindow(512, 512, "6502 Graphics");
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        // 0200 - 05FF :
 
-struct Rect
-{
-    int top;
-    int left;
-    int width;
-    int height;
-};
+        // We're going to get all the entities with circles
+        // and draw these circles on the screen
+        auto const my_colour_id = registry.id<Colour>();
+        
+        auto const my_circle_id = registry.id<Circle>();
+        auto const entities_with_circle = registry.get({*my_circle_id, *my_colour_id});
 
-struct Circle
-{
-    int center_x;
-    int center_y;
-    int radius;
-};
+        // Rendering loop system for the circle
+        for (auto const& entity : entities_with_circle)
+        {
+            //void DrawCircle(int centerX, int centerY, float radius, Color color);
+            std::optional<Circle> draw_circle = registry.get<Circle>(entity);
+            std::optional<Colour> draw_colour = registry.get<Colour>(entity);
+            // DrawCircle(draw_circle->center_x, draw_circle->center_y, draw_circle->radius, colour_table[draw_colour->id]);
+        }
 
-int main(int argc, char** argv)
+        auto const my_rect_id = registry.id<Rect>();
+        auto const entities_with_rect = registry.get({*my_rect_id, *my_colour_id});
+
+        // Rendering loop system for the square
+        for (auto const& entity : entities_with_circle)
+        {
+            //DrawRectangle(int posX, int posY, int width, int height, Color color); 
+            std::optional<Rect> draw_rect = registry.get<Rect>(entity);
+            std::optional<Colour> draw_colour = registry.get<Colour>(entity);
+
+            DrawRectangle(draw_rect->left, draw_rect->top, draw_rect->width, draw_rect->height, colour_table[draw_colour->id]);
+        }
+        EndDrawing();
+
+        // Make sure we only draw at most 30 times per second
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000 / 30));
+    }
+
+    return true;
+}
+
+int main(/*int argc, char** argv*/)
 {
+
+    // auto const type_index = get_type_index<Rect, Rect, Circle, Triangle, Position>();
+
     // Create a registry for a few classes
-    maecs::Registry<Circle, Rect, Position> my_registry;
+    maecs::Registry<Circle, Rect, Position, Colour> my_registry;
     auto const my_rect_id = my_registry.id<Rect>();
     auto const my_circle_id = my_registry.id<Circle>();
     auto const my_position_id = my_registry.id<Position>();
+
+
+    auto const my_id0 = my_registry.bit_mask<Circle>();
+    auto const my_id1 = my_registry.bit_mask<Rect>();
+    auto const my_id2 = my_registry.bit_mask<Position>();
+    auto const my_id3 = my_registry.bit_mask<Colour>();
+
+
+    auto const my_composite_id0 = my_registry.bit_mask<Colour, Rect>();
+    auto const my_composite_id1 = my_registry.bit_mask<Circle, Position, Rect>();
 
 
     // Create a dummy Rectangle component for an entity
@@ -96,7 +143,7 @@ int main(int argc, char** argv)
     Circle const my_circle{
         .center_x = 50,
         .center_y = 50,
-        .radius = 20,       
+        .radius = 20,
     };
 
     Position const my_position{
@@ -113,13 +160,19 @@ int main(int argc, char** argv)
     // - select ents with {position} -> 1, 3
     my_registry.set(0, my_rect);
     my_registry.set(0, my_circle);
+    my_registry.set(0, Colour{.id= 2});
     my_registry.set(1, my_position);
+    my_registry.set(1, Rect{.top = 300, .left = 400, .width = 50, .height = 50});
+    my_registry.set(1, Colour{.id= 10});
     my_registry.set(3, my_position);
     my_registry.set(3, my_rect);
+    my_registry.set(3, Circle{.center_x = 512, .center_y = 512, .radius = 200});
+    my_registry.set(3, Colour{.id= 3});
 
     auto const entities_with_circle = my_registry.get({*my_circle_id});
     auto const entities_with_rect = my_registry.get({*my_rect_id});
     auto const entities_with_pos = my_registry.get({*my_position_id});
 
-    // draw(easy65k);
+
+    draw(my_registry);
 }
--- maecs/maecs.cpp
+++ maecs/maecs.cpp
@@ -1,11 +1,14 @@
 module;
 
+#include <ankerl/unordered_dense.h>
+
 #include <algorithm>
 #include <array>
 #include <cstddef>
 #include <cstdint>
 #include <optional>
 #include <typeinfo>
+#include <type_traits>
 #include <unordered_map>
 #include <utility>
 #include <variant>
@@ -13,15 +16,30 @@ module;
 
 export module maecs;
 
+// What happens if Target is not in List types??
+/// @brief Gets the index of a type in the list of templated types
+/// @tparam Target should be the type we want to find the index for
+/// @tparam ListHead first type in the list of template args
+/// @tparam ...ListTails the remaning type of list args
+/// @return std::size_t that represents index of Target in {ListHead, ListTains...}
+template<typename Target, typename ListHead, typename... ListTails>
+constexpr std::size_t get_type_index()
+{
+    if constexpr (std::is_same<Target, ListHead>::value)
+        return 0;
+    else
+        return 1 + get_type_index<Target, ListTails...>();
+}
+
 template <typename C>
-void add_component_id_one(std::unordered_map<decltype(std::declval<std::type_info>().name()), std::pair<decltype(std::declval<std::type_info>().hash_code()), std::size_t>>& map, std::size_t index)
+void add_component_id_one(ankerl::unordered_dense::map<decltype(std::declval<std::type_info>().name()), std::pair<decltype(std::declval<std::type_info>().hash_code()), std::size_t>>& map, std::size_t index)
 {
     std::type_info const& type_info = typeid(C);
     map.insert({type_info.name(), {type_info.hash_code(), index}});
 }
 
 template <typename C, typename... Cs>
-void add_component_id(std::unordered_map<decltype(std::declval<std::type_info>().name()), std::pair<decltype(std::declval<std::type_info>().hash_code()), std::size_t>>& map, std::size_t index)
+void add_component_id(ankerl::unordered_dense::map<decltype(std::declval<std::type_info>().name()), std::pair<decltype(std::declval<std::type_info>().hash_code()), std::size_t>>& map, std::size_t index)
 {
     add_component_id_one<C>(map, index);
 
@@ -46,6 +64,12 @@ export namespace maecs {
     using EntityId = std::uint64_t;
     using ComponentId = decltype(std::declval<std::type_info>().hash_code());
     using ComponentNameType = decltype(std::declval<std::type_info>().name());
+
+    EntityId generate_entity_id()
+    {
+        static EntityId curr_id = 0;
+        return curr_id++;
+    }
     
     template <typename... Cs>
     using Component = std::variant<Cs...>;
@@ -63,10 +87,46 @@ export namespace maecs {
             add_component_id<Cs...>(_component_ids, 0);
         }
 
+        /// @brief Gets the reference to the component held by an entity
+        /// @tparam C component class to get
+        /// @param entity_id id of the entity
+        /// @return a valid optional containing the component reference if
+        /// the entity has that component, nulopt otherwise
+        template <typename C>
+        std::optional<C> get(EntityId entity_id) const
+        {
+            // Get the component ID for this type
+            static const auto component_id = typeid(C).hash_code();
+            auto const found = _entities.find(component_id);
+            if (found != end(_entities))
+            {
+                // This is returning a vector<{ent_id, variant}>
+                // find the entity in the vector
+                std::vector<std::pair<EntityId, Component<Cs...>>> const& entity_components = found->second;
+                auto const elem = std::find_if(begin(entity_components), end(entity_components), [entity_id](auto const& val){
+                    auto const& [id, component] = val;
+                    return id == entity_id;
+                });
+                // TODO : This is a linear search so its inneficient
+
+                // TODO : should this be an assert??
+                // elem = iter to pair{id, component}
+                if (elem == end(entity_components))
+                {
+                    return std::nullopt;
+                }
+
+                std::variant<Cs...> const& component = elem->second;
+                return std::make_optional<C>(std::get<C>(component));
+            }
+
+            return std::nullopt;
+        }
+
         /// This function will get all the entity Ids
         /// that have the coponent classes "Cs..." attached
         /// to them
-        std::vector<EntityId> get(std::vector<ComponentId> components)
+        std::vector<EntityId> get(std::vector<ComponentId> const& components) const
         {
             if (components.size() < 1) {
                 return {};
@@ -107,7 +167,7 @@ export namespace maecs {
         /// if the component is not valid for this registry, return
         /// std:nullopt
         template <typename C>
-        std::optional<ComponentId> id()
+        std::optional<ComponentId> id() const
         {
             // TODO : probably want a more optimal way
             // to find the idx for the type in the variant
@@ -144,16 +204,39 @@ export namespace maecs {
             _entities.insert({component_hash, {{ent_id, component}}});
         }
 
+        template <typename Head, typename... Tail>
+        constexpr std::size_t bit_mask()
+        {
+            if constexpr (sizeof...(Tail) == 0)
+            {
+                return (1 << get_type_index<Head, Cs...>());
+            }
+            else
+            {
+                return (1 << get_type_index<Head, Cs...>()) | bit_mask<Tail...>();
+            }
+        }
+
     private:
         // This will store the component Ids for each class given as parameter to Registry
         // the value is of type pair<ComponentId, index in variant>
-        std::unordered_map<ComponentNameType, std::pair<ComponentId, std::size_t>> _component_ids;
+        ankerl::unordered_dense::map<ComponentNameType, std::pair<ComponentId, std::size_t>> _component_ids;
 
         // This will store the entity-component data
-        std::unordered_map<ComponentId, std::vector<std::pair<EntityId, Component<Cs...>>>> _entities;
+        ankerl::unordered_dense::map<ComponentId, std::vector<std::pair<EntityId, Component<Cs...>>>> _entities;
+
+        // Wanna create the entity mask -> entity data
+        // 011 -> {ent_id, {c1, c2}} = ent_id has components in indexes 0, 1 of the Cs... and the data is in vector
+        // ankerl::unordered_dense::map<std::uint64_t, std::vector<std::pair<EntityId, std::vector<Component<Cs...>>>>>;
+
+        // 010 -> {ent_id, {c1}}
+        // add c2 to ent_id ?
+        // 110 -> {ent_id, {c1, c2}}
+        // 010 -> {}
 
         // Array to store all the type_ids in order of Components declared in variant
         std::array<std::type_info const*, sizeof...(Cs)> _type_arrays;
+
     };
 
 }
